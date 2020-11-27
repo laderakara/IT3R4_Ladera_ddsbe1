d50dbc3c2ad239348c733577791d813de64d3625
laderakara <karaladera@gmail.com>
2020-11-28 00:51:56 +0800

    nov 28

---
+++ app/Traits/ApiResponser.php
@@ -0,0 +1,17 @@
+<?php
+
+    namespace App\Traits;
+    use Illuminate\Http\Response;
+    Trait ApiResponser{
+
+        Protected function successResponse($data, $code = Response::HTTP_OK)
+        {
+            return response () -> json(['data' => $data, 'site'=> 1],$code);
+        }
+
+        protected function errorResponse($message,$code)
+        {
+            return response()->json(['error'=>$message,'site'=> 1,'code'=>$code],$code); 
+        }
+    }
+?>
